c1008180ef8f77e1c364761f95d7f418695c9e09
christian goddu <rgoddu909@gmail.com>
2023-10-21 01:04:10 -0400

    good progress

--- script.js
+++ script.js
@@ -1,19 +1,64 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-savedTasks = {
+savedTasks = {}
+var saveButton = $(".saveBtn")
+
+function saveContent() {
+
+    var hourSection = $(this).parent()
+    var taskHour = hourSection.attr("id")
+    var taskDescription = hourSection.children().eq(1).val()
+
+    savedTasks[taskHour] = taskDescription;
+    localStorage.setItem("savedTasks", JSON.stringify(savedTasks))
 
 }
+function loadPrevious(){
+    var previousTasks = JSON.parse(localStorage.getItem("savedTasks"))
+    if(previousTasks !== null){
+        for(var x = 0; x<Object.keys(previousTasks).length; x++){
+            var taskKey = Object.keys(previousTasks)[x]
+            var writeArea = $("#"+taskKey)
+            writeArea.children().eq(1).text(previousTasks[taskKey])
+        }
+    }
 
-saveButton = $(".saveBtn")
-function saveContent(){
-    
-console.log($(this).parent().attr("id"))
+}
+function setTime(){
+    var timeDisplay = $("#currentDay")
+    timeDisplay.text(dayjs().format("MMMM DD, YYYY HH:mm:ss"))
+    setInterval(() => {
+        timeDisplay.text(dayjs().format("MMMM DD, YYYY HH:mm:ss"))
+    }, 1000);
+}
+function currentBlock(){
+    for(var x = 9; x<=17; x++){
+        var currentHour = dayjs().format("H")
+        var timeBlock = $("#hour-"+x)
 
+        if(x>currentHour){
+            timeBlock.addClass("future")
+            timeBlock.removeClass("present","past")
+        }else if(x === currentHour){
+            timeBlock.addClass("present")
+            timeBlock.removeClass("future","past")
+        }else{
+            timeBlock.addClass("past")
+            timeBlock.removeClass("future","present")
+        }
+    }
 }
 $(function init() {
+    loadPrevious()
+    setTime()
+    currentBlock()
+    //update block colors every 10 seconds
+    setInterval(()=>{
+        currentBlock()
+    },10000)
     saveButton.on("click", saveContent)
-    
+
 
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener
@@ -32,5 +77,4 @@ $(function init() {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
-  
+});
